bf1e559f0be4e7fb4a9cae77b6da98f6cfee14a7
Cristian Consonni <cristian@balist.es>
2018-07-25 02:05:46 +0200

    Generate cliques with K limited to 5

--- utils/generate_cliques.sh
+++ utils/generate_cliques.sh
@@ -2,9 +2,9 @@
 
 # declare an array called array and define 3 vales
 cliques=( 3 4 5 6 8 10 12 16 32 64 128 256 1024 2048 4096 8192 16384 \
-		  32768 65536 )
+		  32768 )
 for i in "${cliques[@]}"; do
 	echo -n "Generating ${i}-clique..."
-	./clique_generator.py "${i}" -o "../data/toy/cliques/clique-${i}.txt"
+	./clique_generator.py "${i}" -s 0 -l 5 -o "../clique5-${i}.txt"
 	echo ' done!'
 done
