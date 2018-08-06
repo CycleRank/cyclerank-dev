a6decfd67dbb732c35baf12909fe8ac308c1f745
Cristian Consonni <cristian@balist.es>
2018-08-06 20:41:16 +0200

    Add option to limit cycle length

--- compute_scores.py
+++ compute_scores.py
@@ -20,18 +20,27 @@ if __name__ == '__main__':
                         type=pathlib.Path,
                         help='output file name [default: stdout].')
 
+    parser.add_argument('-k', '--maxloop',
+                        type=int,
+                        dest='K',
+                        help='Limit loops to this length (K) '
+                             '[default: No limit].')
+
     args = parser.parse_args()
 
     infile = args.FILE
     output = args.output
+    K = args.K
 
     scores = defaultdict(float)
     with infile.open('r') as infp:
         reader = csv.reader(infp, delimiter=' ')
 
         for cycle in reader:
-            cycle = [int(node) for node in cycle]
             csize = len(cycle)
+            if K is not None and csize > K: continue
+
+            cycle = [int(node) for node in cycle]
 
             for node in cycle:
                 scores[node] += 1.0/csize
