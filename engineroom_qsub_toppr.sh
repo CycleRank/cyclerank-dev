60a10633951e899aea90011889c572c412b7446b
Cristian Consonni <cristian@balist.es>
2019-08-22 19:02:22 +0200

    Fix PBS job name

--- engineroom_qsub_toppr.sh
+++ engineroom_qsub_toppr.sh
@@ -552,7 +552,7 @@ for title in "${!pages[@]}"; do
   idx="${pages[$title]}"
   normtitle="${title/ /_}"
 
-  pbsjobname="lrssppr-toppr_${MAXLOOP}_${PAGERANK_ALPHA}${wholenetwork_flag:+"${wholenetwork_flag}"}_${SCORING_FUNCTION}_${idx}"
+  pbsjobname="lrpr-toppr_${MAXLOOP}_${PAGERANK_ALPHA}${wholenetwork_flag:+"${wholenetwork_flag}"}_${SCORING_FUNCTION}_${idx}"
 
   logfile="${OUTPUTDIR}/${PROJECT}.looprank.${normtitle}.${MAXLOOP}.${DATE}.log"
   echo "Logging to ${logfile}"
