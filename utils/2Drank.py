08512992035f51a4151c78121c83e2be279b53ed
Cristian Consonni <cristian.consonni@unitn.it>
2019-09-22 13:41:32 +0200

    Minor change to work on toy examples

--- utils/2Drank.py
+++ utils/2Drank.py
@@ -26,7 +26,11 @@ regex_score = re.compile(REGEX_SCORE)
 # example name:
 # enwiki.cheir.1999_Bridge_Creek–Moore_tornado.4.2018-03-01.txt
 #
-REGEX_NAME = r'([a-z]{2}wiki)\.cheir\.(.+)\.(.+)\.(\d{4}-\d{2}-\d{2})\.(csv|txt)'
+# REGEX_NAME = r'([a-z]{2}wiki)\.cheir\.(.+)\.(.+)\.(\d{4}-\d{2}-\d{2})\.(csv|txt)'
+#
+# example name
+# toy.cheir.a0.30.input0.wholenetwork.txt
+REGEX_NAME = r'(toy)\.cheir\.(.+)\.(.+)\.(.+)\.(csv|txt)'
 regex_name = re.compile(REGEX_NAME)
 
 
