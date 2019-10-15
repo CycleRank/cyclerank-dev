b18a15392e9d57803e2725fb6e1add84e77343d6
Cristian Consonni <cristian@balist.es>
2019-10-15 23:32:30 +0200

    Add script to calculate SCC on graphs

---
+++ scc.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2128
+SOURCED=false && [ "$0" = "$BASH_SOURCE" ] || SOURCED=true
+
+if ! $SOURCED; then
+  echo 'Enable strict mode'
+  set -euo pipefail
+  IFS=$'\n\t'
+fi
+
+scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
+function finish {
+  rm -rf "$scratch"
+}
+trap finish EXIT
+
+graph="$1"
+outdir="$2"
+
+filename=$(basename -- "${graph}")
+outfile="${filename%.*}.concomp.csv"
+
+echo "Processing ${graph}...";
+data=$(head -n 1 "${graph}" | tr -d $'\r');
+IFS=' ' read -r nodes edges <<< "${data}"
+echo "nodes: $nodes, edges: $edges"
+if [ "$nodes" -gt 0 ]; then
+  cp "${graph}" "${scratch}/${filename}"
+  sed -i '1d' "${scratch}/${filename}"
+
+  /opt/snap/snap/examples/concomp/concomp \
+    "-i:${scratch}/${filename}" \
+    "-o:${scratch}/results.txt"
+
+  cp "${scratch}/results.txt" "${outdir}/${outfile}"
+fi
+
+exit 0
